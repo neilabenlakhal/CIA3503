a34136b050300c70ec2c8e741dc5411c1bd28afc
Neila Ben Lakhal <nlakhal@hct.ac.ae>
2024-10-10 13:56:01 +0400

    Activity 6 updated

--- Activity6-GUI-Complete/src/main/java/edu/programsmanagement/dao/CourseDAO.java
+++ Activity6-GUI-Complete/src/main/java/edu/programsmanagement/dao/CourseDAO.java
@@ -1,12 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package edu.programsmanagement.dao;
 
 /**
  *
- * @author nbenayed
  */
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -17,183 +12,204 @@ import java.util.ArrayList;
 import java.util.List;
 import edu.programsmanagement.model.Course;
 import edu.programsmanagement.model.Program;
+
 public class CourseDAO {
 
-//	findCourseByCode(String code): Course method takes a program code as input and returns the course object if course code exists in the DB.    
-    public Course findCourseByCode(String code) throws SQLException{
-        Connection conn=DBConnection.getConnection();
-        
-        String sql="SELECT * FROM course WHERE code=?;";
-        PreparedStatement pstmt=conn.prepareStatement(sql);
-        pstmt.setString(1,code);
-        
-        ResultSet rs=pstmt.executeQuery();
-        Course course=null;
-        if(rs.next()){
-            String title=rs.getString("title");
-            int credits=rs.getInt("credits");
-            String progCode=rs.getString("Program_Code");
-            ProgramDAO programDAO=new ProgramDAO();
+    public Connection conn;
+    public Statement stmt;
+    public PreparedStatement pstmt;
+    public String sql;
+    public ResultSet rs;
+    public int status;
+    public Course course = null;
+    public Program program = null;
+    public ProgramDAO programDAO = new ProgramDAO();
+    public ArrayList<Course> courseList = new ArrayList<>();
 
-            Program program=programDAO.findProgramByCode(progCode); 
-            course=new Course(code,title,credits, program);
-            
+    // This method will check if the course exists 
+    public boolean existCourse(String code) throws SQLException {
+        conn = DBConnection.getConnection();
+        boolean found = false;
+
+        String query = "SELECT code FROM course WHERE code=?;";
+        pstmt = conn.prepareStatement(query);
+        pstmt.setString(1, code);
+        rs = pstmt.executeQuery();
+
+        if (rs.next()) {
+            String cc = rs.getString("code");
+            if (cc != null) {
+                found = true;
+            }
         }
-        
+        pstmt.close();
+        rs.close();
+        conn.close();
+        return found;
+    }
+
+    public List<Course> getCoursesByProgram(String programTitle) throws SQLException {
+
+        conn = DBConnection.getConnection();
+        sql = "select * from Course c, Program p where c.program_code = p.code and p.title=? ;";
+        pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1, programTitle);
+        rs = pstmt.executeQuery();
+
+        while (rs.next()) {
+            String cc = rs.getString("code");
+            int credits = rs.getInt("credits");
+            String code = rs.getString("program_code");
+            String title = rs.getString("title");
+            String acc = rs.getString("accreditation");
+
+            program = new Program(code, programTitle, acc);
+            course = new Course(code, title, credits, program);
+
+            courseList.add(course);
+        }
+        rs.close();
+        pstmt.close();
+        conn.close();
+        return courseList;
+    }
+
+//findCourseByCode(String code): Course method takes a program code as 
+//input and returns the course object if course code exists in the DB.    
+    public Course findCourseByCode(String code) throws SQLException {
+        conn = DBConnection.getConnection();
+        sql = "SELECT * FROM course WHERE code=?;";
+        pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1, code);
+
+        rs = pstmt.executeQuery();
+
+        if (rs.next()) {
+            String title = rs.getString("title");
+            int credits = rs.getInt("credits");
+            String progCode = rs.getString("Program_Code");
+            program = programDAO.findProgramByCode(progCode);
+            course = new Course(code, title, credits, program);
+
+        }
+
         rs.close();
         pstmt.close();
         conn.close();
         return course;
     }
 
+    public List<Course> getAllCourses() throws SQLException {
+        conn = DBConnection.getConnection();
+        sql = "SELECT * FROM course;";
+        pstmt = conn.prepareStatement(sql);
 
-    public List<Course> getAllCourses() throws SQLException{
-        Connection conn=DBConnection.getConnection();
-    
-        Statement stmt=conn.createStatement();
-        
-        String sql="SELECT * FROM course;";
-        PreparedStatement pstmt=conn.prepareStatement(sql);
-        
-        ResultSet rs=pstmt.executeQuery();
-        Course course;
-        ArrayList<Course> courseList=new ArrayList<Course>();
-        while(rs.next()){
-            String code=rs.getString("code");
-            String title=rs.getString("title");
-            int credits=rs.getInt("credits");
-            String progCode=rs.getString("program_code");
-        
-           //create an object prog with the data retrieved from the resultset
-            ProgramDAO programDAO=new ProgramDAO();
+        rs = pstmt.executeQuery();
+        while (rs.next()) {
+            String code = rs.getString("code");
+            String title = rs.getString("title");
+            int credits = rs.getInt("credits");
+            String progCode = rs.getString("program_code");
 
-            Program program=programDAO.findProgramByCode(progCode); 
-            course=new Course(code,title,credits, program);
+            //create an object prog with the data retrieved from the resultset
+            program = programDAO.findProgramByCode(progCode);
+            course = new Course(code, title, credits, program);
             //add prog to progList
             courseList.add(course);
-            
         }
-        
         //setp4: close the connection
         rs.close();
         pstmt.close();
         conn.close();
         return courseList;
     }
-    
 
     //getAllCourses(String progCode): List<Courses> method takes a program code as input and returns the list of all courses of the given program.
-    public List<Course>  getAllCourses(String progCode) throws SQLException{
-        Connection conn=DBConnection.getConnection();
-        
-        //step2: create a prepared statement
-        String sql="SELECT * FROM course where program_code=?;";
-        PreparedStatement pstmt=conn.prepareStatement(sql);
-        pstmt.setString(1,progCode);
-        
-        
-        //step3: execute the preapared statement
-        ResultSet rs=pstmt.executeQuery();
-        Course course;
-        ArrayList<Course> courseList=new ArrayList<Course>();
-        while(rs.next()){
-            String code=rs.getString("code");
-            String title=rs.getString("title");
-            int credits=rs.getInt("credits");
-            ProgramDAO programDAO=new ProgramDAO();
+    public List<Course> getAllCourses(String progCode) throws SQLException {
+        conn = DBConnection.getConnection();
 
-            Program program=programDAO.findProgramByCode(progCode); 
-            course=new Course(code,title,credits, program);
-            
-            
-           
+        //step2: create a prepared statement
+        sql = "SELECT * FROM course where program_code=?;";
+        pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1, progCode);
+
+        //step3: execute the preapared statement
+        rs = pstmt.executeQuery();
+        while (rs.next()) {
+            String code = rs.getString("code");
+            String title = rs.getString("title");
+            int credits = rs.getInt("credits");
+            program = programDAO.findProgramByCode(progCode);
+            course = new Course(code, title, credits, program);
             courseList.add(course);
-            
         }
-        
         rs.close();
         pstmt.close();
         conn.close();
-        
         return courseList;
     }
 
-    
     //d.	addNewCourse(String code, String title, int credits, String progCode):int method takes the code, title, credits, and progCode as input, adds the course into the database, and returns the number of rows affected.
-    public int addNewCourse(String code, String title, int credits, String progCode) throws SQLException{
-                 
-        //step1: connect to the database
-        Connection conn=DBConnection.getConnection();
-    
-    
-       //step2: create a prepared statement
-        String sql="INSERT INTO COURSE VALUES(?,?,?,?);";
-        PreparedStatement pstmt=conn.prepareStatement(sql);
-        pstmt.setString(1,code);
-        pstmt.setString(2,title);
-        pstmt.setInt(3,credits);
-        pstmt.setString(4,progCode);
-        //step3: execute the query 
-        int insertStatus= pstmt.executeUpdate();
-        //setp4: close the connection
-        pstmt.close();
-        conn.close();
-        return insertStatus;
-        
-       
-    }
-    
-//e.	updateCourse(String code, String title, int credits, String progCode):int method takes the code, title, credits, and progCode as input, updates the title, credits, and program code of a given course code, and returns the number of rows affected.
-    
-    public int updateCourse(String code, String title, int credits, String progCode) throws SQLException{
-    
-        //step1: connect to the database
-        Connection conn=DBConnection.getConnection();
-    
-        //step2: create the preparedstatement
-       String sql="UPDATE COURSE SET title=?,credits=?, program_code=? WHERE code=?;";
+    public int addNewCourse(String code, String title, int credits, String progCode) throws SQLException {
 
-       PreparedStatement pstmt=conn.prepareStatement(sql);
-       
-   
-       pstmt.setString(1, title);
-       pstmt.setInt(2,credits);
-       pstmt.setString(3, progCode);
-       pstmt.setString(4, code);
-
-       
-       
-        //step3: execute the statment
-                
-               int updateStatus= pstmt.executeUpdate();
-        //setp4: close the connection
-        pstmt.close();
-        conn.close();
-        return updateStatus;
-    
-    }
- 
-//f.	deleteCourse(String code):int method takes code as input, delete the course for the given code from the database, and returns the number of rows affected.
-    
-    public int deleteCourse(String code) throws SQLException{ 
         //step1: connect to the database
-        Connection conn=DBConnection.getConnection();
+        conn = DBConnection.getConnection();
+
         //step2: create a prepared statement
-        String sql="DELETE FROM COURSE WHERE CODE=?;";
-        PreparedStatement pstmt=conn.prepareStatement(sql);
-        pstmt.setString(1,code);
-        //step3: execute the statement
-        int deleteStatus= pstmt.executeUpdate();
+        sql = "INSERT INTO COURSE VALUES(?,?,?,?);";
+        pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1, code);
+        pstmt.setString(2, title);
+        pstmt.setInt(3, credits);
+        pstmt.setString(4, progCode);
+        //step3: execute the query 
+        status = pstmt.executeUpdate();
         //setp4: close the connection
         pstmt.close();
         conn.close();
-    return deleteStatus;
+        return status;
+
     }
-    
-    
-    
-    
 
+//e.	updateCourse(String code, String title, int credits, String progCode):int method takes the code, title, credits, and progCode as input, updates the title, credits, and program code of a given course code, and returns the number of rows affected.
+    public int updateCourse(String code, String title, int credits, String progCode) throws SQLException {
 
-    
+        //step1: connect to the database
+        conn = DBConnection.getConnection();
+
+        //step2: create the preparedstatement
+        sql = "UPDATE COURSE SET title=?,credits=?, program_code=? WHERE code=?;";
+
+        pstmt = conn.prepareStatement(sql);
+
+        pstmt.setString(1, title);
+        pstmt.setInt(2, credits);
+        pstmt.setString(3, progCode);
+        pstmt.setString(4, code);
+
+        //step3: execute the statment
+        int updatestatus = pstmt.executeUpdate();
+        //setp4: close the connection
+        pstmt.close();
+        conn.close();
+        return status;
+
+    }
+
+//f.	deleteCourse(String code):int method takes code as input, delete the course for the given code from the database, and returns the number of rows affected.
+    public int deleteCourse(String code) throws SQLException {
+        //step1: connect to the database
+        conn = DBConnection.getConnection();
+        //step2: create a prepared statement
+        sql = "DELETE FROM COURSE WHERE CODE=?;";
+        pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1, code);
+        //step3: execute the statement
+        status = pstmt.executeUpdate();
+        //setp4: close the connection
+        pstmt.close();
+        conn.close();
+        return status;
+    }
+
 }
